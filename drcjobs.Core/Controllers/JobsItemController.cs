76a1cefd5a489df31903e23d100574427fe48764
Aleksander Fjellvang <afj@dwarf.dk>
2024-01-31 11:42:53 +0100

    FIX: Handle job page renders where no id is passed

--- drcjobs.Core/Controllers/JobsItemController.cs
+++ drcjobs.Core/Controllers/JobsItemController.cs
@@ -1,12 +1,17 @@
 ﻿using System.Web.Mvc;
+using Umbraco.Web;
 using Umbraco.Web.Mvc;
 
 namespace drcjobs.Core.Controllers
 {
     public class JobsItemController : RenderMvcController
     {
-        public ActionResult Index(int id)
+        public ActionResult Index(int? id)
         {
+            if (!id.HasValue)
+            {
+                return new RedirectResult(CurrentPage.Parent.Url(), false);
+            }
             ViewBag.PositionId = id;
             return CurrentTemplate(CurrentPage);
         }
